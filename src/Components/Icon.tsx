0cb996ee74e8516200b79e98540943207dcf62d0
aylailoveyou <129250367+aylailoveyou@users.noreply.github.com>
2023-03-29 02:44:45 -0300

    Update Icon.tsx

--- src/Components/Icon.tsx
+++ src/Components/Icon.tsx
@@ -4,7 +4,7 @@ const { Tooltip } = components;
 function Icon(path: string, viewBox = "0 0 24 24") {
   return ({ color, tooltip, className }: { color: string; tooltip: string; className: string }) => (
     <Tooltip text={tooltip}>
-      <svg className={className} height="20" width="20" viewBox={viewBox} fill={color}>
+      <svg className={className} height="10" width="10" viewBox={viewBox} fill={color}>
         <path d={path} />
       </svg>
     </Tooltip>
